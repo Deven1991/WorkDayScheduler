32be92e9441656b3f55e96846782e7cec39ac667
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-05-03 19:33:15 -0500

    final version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
     var Today = moment().format("dddd, MMMM Do, YYYY");
-    $("#currentDay").text(Today);
+    $("#isToday").text(Today);
 
     $("saveBtn").on("click", function () {
-        var inputText = $(this).siblings(".description").val();
+        var inputText = $(this).siblings(".schedule").val();
         var timeStamp = $(this).parent().attr("id");
 
         //saves items in local storage
         localStorage.listItem(inputText, timeStamp);
     })
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .schedule").val(localStorage.getItem("hour9"));
+    $("#hour10 .schedule").val(localStorage.getItem("hour10"));
+    $("#hour11 .schedule").val(localStorage.getItem("hour11"));
+    $("#hour12 .schedule").val(localStorage.getItem("hour12"));
+    $("#hour13 .schedule").val(localStorage.getItem("hour13"));
+    $("#hour14 .schedule").val(localStorage.getItem("hour14"));
+    $("#hour15 .schedule").val(localStorage.getItem("hour15"));
+    $("#hour16 .schedule").val(localStorage.getItem("hour16"));
+    $("#hour17 .schedule").val(localStorage.getItem("hour17"));
 
     function currentHours() {
         //get current number of hours.
